85213d6dfe3724c4b7a0bcfd2da762c3d16e4d75
Ross-Hunter <ross-hunter@ross-hunter.com>
2021-12-16 15:09:20 -0500

    only bother supporting our version of rails

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -5,6 +5,7 @@ set -e
 gem install rubocop
 # TODO: enable this on our repo and use it
 # gem install rubocop-performance
+gem install rails -v 5.2.6
 gem install rubocop-rails
 
 ruby /action/lib/index.rb
